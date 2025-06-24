7719b8206892a818a810340daa0d04afb2b62437
Zanix <88017818+zanix-ai@users.noreply.github.com>
2025-06-25 04:28:09 +0700

    Update bgm.js

--- bgm.js
+++ bgm.js
@@ -1,31 +1,7 @@
-let bgmList = [];
-let currentIndex = 0;
-
-fetch('bgm.txt')
-  .then(res => res.text())
-  .then(text => {
-    bgmList = text.trim().split('\n').filter(x => x.trim());
-    if (bgmList.length) playBGM(currentIndex);
-  });
-
-function playBGM(index) {
-  if (!bgmList.length) return;
-  currentIndex = (index + bgmList.length) % bgmList.length;
-  const url = bgmList[currentIndex];
-
-  try {
-    const videoId = new URL(url).searchParams.get("v");
-    const iframe = document.getElementById("bgmPlayer");
-    iframe.src = `https://www.youtube.com/embed/${videoId}?autoplay=1&loop=1&playlist=${videoId}&controls=0`;
-  } catch (e) {
-    console.error("Invalid URL:", url);
-  }
-}
-
-function nextBGM() {
-  playBGM(currentIndex + 1);
-}
-
-function prevBGM() {
-  playBGM(currentIndex - 1);
-}
+const bgmList = [
+ "https://youtu.be/ckamEBDt0Dg?si=94sv8vfleNJPA2gJ",
+ "https://youtu.be/O2ct-cdupHU?si=XkpaFX3y2XVKf2Vd",
+ "https://youtu.be/VbmAs3dpOyI?si=6KgH7hGgEwLQm_lE",
+ "https://youtu.be/U427Icn6Vfs?si=ZTM4YB-9em3IEMEX",
+ "https://youtu.be/dtl4UIcKyww?si=xP6p9Dpp_srxfyCu"
+];
